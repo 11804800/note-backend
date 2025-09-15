c0c6f7cb0583bdf19300473ecd1adf6117576fcf
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-09-15 20:02:55 +0530

    Vercel Cors Error fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
 const app = express();
 
 app.use(cors({
-  origin: 'https://note-frontend-pi.vercel.app', 
+  origin: 'https://note-frontend-alpha.vercel.app/', 
   methods: ['POST',"GET","PUT","DELETE", 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,
